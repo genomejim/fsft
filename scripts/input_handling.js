fd09db50aa52423e7ae0331701b2db26f9163a2b
Jim Eldred <gscjim@gmail.com>
2013-02-03 11:31:38 -0600

    labels for the kinetic buttons

--- scripts/input_handling.js
+++ scripts/input_handling.js
@@ -35,6 +35,14 @@ function down(event) {
     strokeWidth: 2
 
   });
+  var text1 = new Kinetic.Text({
+        x: 110,
+        y: 15,
+        text: 'scientist',
+        fontSize: 15,
+        fontFamily: 'Calibri',
+        fill: 'white'
+  });
 
   var rect2 = new Kinetic.Rect({
 
@@ -46,6 +54,14 @@ function down(event) {
     stroke: "black",
     strokeWidth: 2
 
+  });
+  var text2 = new Kinetic.Text({
+        x: 200,
+        y: 15,
+        text: 'trooper',
+        fontSize: 15,
+        fontFamily: 'Calibri',
+        fill: 'white'
   });
 
   var rect3 = new Kinetic.Rect({
@@ -60,6 +76,15 @@ function down(event) {
 
   });
 
+  var text3 = new Kinetic.Text({
+        x: 290,
+        y: 15,
+        text: 'pylon',
+        fontSize: 15,
+        fontFamily: 'Calibri',
+        fill: 'white'
+  });
+
   var rect4 = new Kinetic.Rect({
 
     x: 370,
@@ -71,6 +96,16 @@ function down(event) {
     strokeWidth: 2
 
   });
+
+  var text4 = new Kinetic.Text({
+        x: 380,
+        y: 15,
+        text: 'giant trpr',
+        fontSize: 15,
+        fontFamily: 'Calibri',
+        fill: 'white'
+  });
+
   var rect5 = new Kinetic.Rect({
 
     x: 460,
@@ -81,6 +116,15 @@ function down(event) {
     stroke: "black",
     strokeWidth: 2
 
+  });
+
+  var text5 = new Kinetic.Text({
+        x: 470,
+        y: 15,
+        text: 'grogon',
+        fontSize: 15,
+        fontFamily: 'Calibri',
+        fill: 'white'
   });
 
   var rect6 = new Kinetic.Rect({
@@ -94,15 +138,28 @@ function down(event) {
     strokeWidth: 2
 
   });
-
+  var text6 = new Kinetic.Text({
+        x: 560,
+        y: 15,
+        text: 'rocket',
+        fontSize: 15,
+        fontFamily: 'Calibri',
+        fill: 'white'
+  });
 
       // add the shape to the layer
       button_layer.add(rect1);
+      button_layer.add(text1);
       button_layer.add(rect2);
+      button_layer.add(text2);
       button_layer.add(rect3);
+      button_layer.add(text3);
       button_layer.add(rect4);
+      button_layer.add(text4);
       button_layer.add(rect5);
+      button_layer.add(text5);
       button_layer.add(rect6);
+      button_layer.add(text6);
             
 
       // add the layer to the stage
@@ -125,5 +182,23 @@ function down(event) {
         add_unit('grogon');
       });
       rect6.on('mousedown click tap', function() {
+        add_unit('rocket');
+      });
+      text1.on('mousedown click tap', function() {
+        add_unit('scientist');
+      });
+      text2.on('mousedown click tap', function() {
+        add_unit('science_trooper');
+      });
+      text3.on('mousedown click tap', function() {
+        add_unit('pylon');
+      });
+      text4.on('mousedown click tap', function() {
+        add_unit('giant_trooper');
+      });
+      text5.on('mousedown click tap', function() {
+        add_unit('grogon');
+      });
+      text6.on('mousedown click tap', function() {
         add_unit('rocket');
       });
